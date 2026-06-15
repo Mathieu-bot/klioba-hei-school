dbf2047597198251a6377fc8ab728241ba9ea93a
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-15 22:34:42 +0300

    refactor: rename donor to member

--- src/main/java/school/hei/tsinjo/model/User.java
+++ src/main/java/school/hei/tsinjo/model/User.java
@@ -7,7 +7,7 @@ import lombok.ToString;
 @ToString
 @Getter
 @AllArgsConstructor
-public sealed class User permits Donor, WithDrawer {
+public sealed class User permits Member, WithDrawer {
   private final String id;
   private final String firstName;
   private final String lastName;
