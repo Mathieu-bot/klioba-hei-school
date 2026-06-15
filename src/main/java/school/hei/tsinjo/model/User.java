19f17151ec31f319a716b3e4a99df12519453297
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-15 23:15:15 +0300

    refactor: update mappers to follow new jpa entities

--- src/main/java/school/hei/tsinjo/model/User.java
+++ src/main/java/school/hei/tsinjo/model/User.java
@@ -1,5 +1,6 @@
 package school.hei.tsinjo.model;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
@@ -12,4 +13,5 @@ public sealed class User permits Member, WithDrawer {
   private final String firstName;
   private final String lastName;
   private final String email;
+  private final List<Club> clubs;
 }
