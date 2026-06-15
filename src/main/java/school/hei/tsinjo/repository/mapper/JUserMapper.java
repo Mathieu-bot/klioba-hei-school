6643cc167bb7dd9bb52db3774cc6d23287a401e2
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-15 23:28:03 +0300

    refactor: update j user mapper not to take off list of users when mapping user to j user

--- src/main/java/school/hei/tsinjo/repository/mapper/JUserMapper.java
+++ src/main/java/school/hei/tsinjo/repository/mapper/JUserMapper.java
@@ -1,38 +1,19 @@
 package school.hei.tsinjo.repository.mapper;
 
 import java.util.ArrayList;
-import java.util.stream.Collectors;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import school.hei.tsinjo.model.User;
 import school.hei.tsinjo.repository.jpa.model.JUser;
 
 @Component
-@AllArgsConstructor
 public class JUserMapper {
 
-  private final JClubMapper jClubMapper;
-
   public User toDomain(JUser jUser) {
-    var clubs = jUser.getClubs().stream().map(jClubMapper::toDomain).collect(Collectors.toList());
-
-    return new User(
-        jUser.getId(), jUser.getFirstName(), jUser.getLastName(), jUser.getEmail(), clubs);
+    return new User(jUser.getId(), jUser.getFirstName(), jUser.getLastName(), jUser.getEmail());
   }
 
   public JUser toEntity(User user) {
-    var jClubs = user.getClubs().stream().map(jClubMapper::toEntity).collect(Collectors.toList());
-
-    var jUser =
-        new JUser(
-            user.getId(),
-            user.getEmail(),
-            user.getFirstName(),
-            user.getLastName(),
-            new ArrayList<>());
-
-    jUser.setClubs(jClubs);
-
-    return jUser;
+    return new JUser(
+        user.getId(), user.getEmail(), user.getFirstName(), user.getLastName(), new ArrayList<>());
   }
 }
