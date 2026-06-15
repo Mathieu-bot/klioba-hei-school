97f09e4a7f7afa83d85ec04292c2ae949016107c
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-15 22:37:20 +0300

    refactor: add club field to event

--- src/main/java/school/hei/tsinjo/model/Event.java
+++ src/main/java/school/hei/tsinjo/model/Event.java
@@ -12,14 +12,15 @@ public abstract sealed class Event permits Donation, Help {
   protected final String id;
   protected final Payment payment;
   protected final User user;
+  protected final Club club;
   protected final Instant creationInstant;
   protected final String comment;
 
   public static Event from(
-      String id, Payment payment, User user, Instant creationInstant, String comment) {
+      String id, Payment payment, User user, Club club,Instant creationInstant, String comment) {
     return payment.amount() == null || payment.amount() >= 0
-        ? new Donation(id, payment, user, creationInstant)
-        : new Help(id, payment, user, creationInstant, comment);
+        ? new Donation(id, payment, user, club,creationInstant)
+        : new Help(id, payment, user, club,creationInstant, comment);
   }
 
   public abstract Event withPayment(Payment newPayment);
