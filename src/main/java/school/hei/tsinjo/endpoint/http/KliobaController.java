357a86e039b17f279c3f73c2c40ab5f640a77f1e
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-06-16 09:59:55 +0300

    fix : format kliobaController.java

--- src/main/java/school/hei/tsinjo/endpoint/http/KliobaController.java
+++ src/main/java/school/hei/tsinjo/endpoint/http/KliobaController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import school.hei.tsinjo.endpoint.http.model.ThEvent;
 import school.hei.tsinjo.endpoint.http.model.ThFund;
 import school.hei.tsinjo.service.EventService;
