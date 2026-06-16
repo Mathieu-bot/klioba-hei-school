30411e4f169f8fcfcdef6a4d2a26bdf47745962d
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-16 09:48:03 +0300

    refactor: propagate logic translation

--- src/main/java/school/hei/tsinjo/model/MembershipFee.java
+++ src/main/java/school/hei/tsinjo/model/MembershipFee.java
@@ -2,13 +2,13 @@ package school.hei.tsinjo.model;
 
 import java.time.Instant;
 
-public final class Donation extends Event {
-  public Donation(String id, Payment payment, User user, Instant creationInstant) {
-    super(id, payment, user, creationInstant, "");
+public final class MembershipFee extends Event {
+  public MembershipFee(String id, Payment payment, User user, Club club, Instant creationInstant) {
+    super(id, payment, user, club, creationInstant, "");
   }
 
   @Override
   public Event withPayment(Payment newPayment) {
-    return new Donation(id, newPayment, user, creationInstant);
+    return new MembershipFee(id, newPayment, user, club, creationInstant);
   }
 }
--- src/main/java/school/hei/tsinjo/model/Withdrawal.java
+++ src/main/java/school/hei/tsinjo/model/Withdrawal.java
@@ -2,13 +2,14 @@ package school.hei.tsinjo.model;
 
 import java.time.Instant;
 
-public final class Help extends Event {
-  public Help(String id, Payment payment, User user, Instant creationInstant, String comment) {
-    super(id, payment, user, creationInstant, comment);
+public final class Withdrawal extends Event {
+  public Withdrawal(
+      String id, Payment payment, User user, Club club, Instant creationInstant, String comment) {
+    super(id, payment, user, club, creationInstant, comment);
   }
 
   @Override
   public Event withPayment(Payment newPayment) {
-    return new Help(id, newPayment, user, creationInstant, comment);
+    return new Withdrawal(id, newPayment, user, club, creationInstant, comment);
   }
 }
